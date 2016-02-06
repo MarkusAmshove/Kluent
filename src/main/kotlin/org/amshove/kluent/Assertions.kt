5a75421eae2f327ba977e080e409d9815939a01b
Markus Amshove <scm@amshove.org>
2016-02-06 10:31:45 +0100

    add should containcomparison for collections

--- src/main/kotlin/org/amshove/kluent/Assertions.kt
+++ src/main/kotlin/org/amshove/kluent/Assertions.kt
@@ -7,4 +7,9 @@ infix fun Any.`should equal`(theOther: Any) = if (this == theOther) Unit else fa
 
 infix fun Any.`should be`(theOther: Any) = if (this === theOther) Unit else fail("$this should be the same object as $theOther", "$theOther", "$this")
 
+infix fun <T> Array<T>.`should contain`(theThing: T) = if (this.contains(theThing)) Unit else fail("$this should contain $theThing", "$theThing", "${join(this)}")
+infix fun <T> Iterable<T>.`should contain`(theThing: T) = if (this.contains(theThing)) Unit else fail("$this should contain $theThing", "$theThing", "${join(this)}")
+
 private fun fail(message: String, expected: String, actual: String): Nothing = throw ComparisonFailure(message, expected, actual)
+private fun <T> join(theArray: Array<T>) : String = theArray.joinToString(", ")
+private fun <T> join(theIterable: Iterable<T>) : String = theIterable.joinToString(", ")
--- src/test/kotlin/org/amshove/kluent/tests/AssertionsShould.kt
+++ src/test/kotlin/org/amshove/kluent/tests/AssertionsShould.kt
@@ -1,12 +1,26 @@
 package org.amshove.kluent.tests
 
 import org.amshove.kluent.`should be`
+import org.amshove.kluent.`should contain`
 import org.amshove.kluent.`should equal`
 import org.amshove.kluent.tests.helpclasses.Person
 import org.jetbrains.spek.api.Spek
+import org.junit.ComparisonFailure
+import kotlin.test.assertEquals
 import kotlin.test.assertFails
 
 class AssertionsShould : Spek() {
+    fun getFailure(func: () -> Unit): ComparisonFailure {
+        try {
+            func.invoke()
+            throw Exception("Test didn't fail")
+        } catch (f: ComparisonFailure) {
+            return f
+        } catch (e: Exception) {
+            throw Exception("Test didn't fail")
+        }
+    }
+
     init {
         given("the should equal method") {
             on("checking equality of two equal strings") {
@@ -28,7 +42,7 @@ class AssertionsShould : Spek() {
                 val firstObject = Person("Jon", "Doe")
                 val secondObject = Person("Jon", "Doe")
                 it("should pass") {
-                   firstObject `should equal` secondObject
+                    firstObject `should equal` secondObject
                 }
             }
         }
@@ -45,11 +59,51 @@ class AssertionsShould : Spek() {
                 val firstObject = Person("Foo", "Bar")
                 val secondObject = Person("Foo", "Bar")
                 it("should fail") {
-                    assertFails({firstObject `should be` secondObject})
+                    assertFails({ firstObject `should be` secondObject })
+                }
+            }
+        }
+
+        given("the should contain method") {
+            on("checking an array with the value") {
+                val array = arrayOf("Hello", "World", "Wide", "Web")
+                it("should pass") {
+                    array `should contain` "Wide"
+                }
+            }
+            on("checking an array without the value") {
+                val array = arrayOf("Hello", "Wide", "Web")
+                it("should fail") {
+                    assertFails({ array `should contain` "World" })
+                }
+                it("should format the array") {
+                    val theFailure = getFailure { array `should contain` "World" }
+                    assertEquals("Hello, Wide, Web", theFailure.actual)
+                }
+            }
+            on("checking any iterable for an object") {
+                val alice = Person("Alice", "Bob")
+                val jon = Person("Jon", "Doe")
+                val list = listOf(alice, jon)
+                it("should pass") {
+                    list `should contain` jon
+                }
+            }
+            on("checking any iterable for an object which isn't contained") {
+                val alice = Person("Alice", "Bob")
+                val jon = Person("Jon", "Doe")
+                val list = listOf(alice)
+                it("should fail") {
+                    assertFails({list `should contain` jon})
+                }
+                it("should format the output") {
+                    val failure = getFailure { list `should contain` jon }
+                    assertEquals("Person(name=Alice, surname=Bob)", failure.actual)
                 }
             }
         }
     }
 }
+
 
 
