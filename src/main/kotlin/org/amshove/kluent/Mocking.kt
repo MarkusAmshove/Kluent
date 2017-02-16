fa475084ad7567bf32e414294f02bfebb67add05
Markus Amshove <scm@amshove.org>
2017-02-16 17:34:05 +0100

    Reformat Exceptions

--- src/main/kotlin/org/amshove/kluent/Mocking.kt
+++ src/main/kotlin/org/amshove/kluent/Mocking.kt
@@ -10,18 +10,26 @@ fun <T : Any> mock(targetClass: KClass<T>): T {
 }
 
 infix fun <T> VerifyKeyword.on(mock: T) = verify(mock)
+
 infix fun <T> T.that(mock: T): T = this.apply { mock.run { Unit } }
+
 infix fun <T> VerifyNoInteractionsKeyword.on(mock: T) = verifyZeroInteractions(mock)
+
 infix fun <T> VerifyNoFurtherInteractionsKeyword.on(mock: T) = verifyNoMoreInteractions(mock)
+
 infix fun <T> T.was(n: CalledKeyword) = n
+
 fun <T : Any> any(kClass: KClass<T>): T = any(kClass.javaObjectType)
 
-infix fun <T> OngoingStubbing<T>.itReturns(value: T): OngoingStubbing<T> = this.thenReturn(value)
 infix fun <T> OngoingStubbing<T>.`it returns`(value: T): OngoingStubbing<T> = this.thenReturn(value)
-infix fun <T> OngoingStubbing<T>.itThrows(value: RuntimeException): OngoingStubbing<T> = this.thenThrow(value)
+infix fun <T> OngoingStubbing<T>.itReturns(value: T): OngoingStubbing<T> = this `it returns` value
+
 infix fun <T> OngoingStubbing<T>.`it throws`(value: RuntimeException): OngoingStubbing<T> = this.thenThrow(value)
-infix fun <T> OngoingStubbing<T>.itAnswers(value: (InvocationOnMock) -> T): OngoingStubbing<T> = this.thenAnswer(value)
+infix fun <T> OngoingStubbing<T>.itThrows(value: RuntimeException): OngoingStubbing<T> = this `it throws` value
+
 infix fun <T> OngoingStubbing<T>.`it answers`(value: (InvocationOnMock) -> T): OngoingStubbing<T> = this.thenAnswer(value)
+infix fun <T> OngoingStubbing<T>.itAnswers(value: (InvocationOnMock) -> T): OngoingStubbing<T> = this `it answers` value
+
 infix fun <T> WhenKeyword.calling(methodCall: T): OngoingStubbing<T> = `when`(methodCall)
 
 
