e60efc9e60982d11961f77b6f3f6e77095388706
Markus Amshove <scm@amshove.org>
2017-02-19 16:09:03 +0100

    Add shouldBeEmpty and shouldNotBeEmpty

--- src/main/kotlin/org/amshove/kluent/Collections.kt
+++ src/main/kotlin/org/amshove/kluent/Collections.kt
@@ -1,7 +1,6 @@
 package org.amshove.kluent
 
-import org.junit.Assert.assertArrayEquals
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 
 infix fun <T> Array<T>.shouldContain(theThing: T) = this `should contain` theThing
 infix fun <T> Array<T>.`should contain`(theThing: T) = if (this.contains(theThing)) Unit else fail("$this should contain $theThing", "$theThing", join(this))
@@ -12,6 +11,12 @@ infix fun <T> Array<T>.shouldNotContain(theThing: T) = this `should not contain`
 infix fun <T> Array<T>?.`should equal`(theOther: Array<T>?) = assertArrayEquals(theOther, this)
 infix fun <T> Array<T>?.shouldEqual(theOther: Array<T>?) = this `should equal` theOther
 
+fun <T> Array<T>.`should be empty`() = assertEmpty(this.toList(), "Array")
+fun <T> Array<T>.shouldBeEmpty() = this.`should be empty`()
+
+fun <T> Array<T>.`should not be empty`() = assertNotEmpty(this.toList(), "Array")
+fun <T> Array<T>.shouldNotBeEmpty() = this.`should not be empty`()
+
 infix fun <T> Iterable<T>.`should contain`(theThing: T) = if (this.contains(theThing)) Unit else fail("$this should contain $theThing", "$theThing", join(this))
 infix fun <T> Iterable<T>.shouldContain(theThing: T) = this `should contain` theThing
 
@@ -20,6 +25,12 @@ infix fun <T> Iterable<T>.shouldNotContain(theThing: T) = this `should not conta
 
 infix fun <T> Iterable<T>?.`should equal`(theOther: Iterable<T>?) = assertEquals(theOther, this)
 infix fun <T> Iterable<T>?.shouldEqual(theOther: Iterable<T>?) = this `should equal` theOther
+
+fun <T> Iterable<T>.`should be empty`() = assertEmpty(this, "Iterable")
+fun <T> Iterable<T>.shouldBeEmpty() = this.`should be empty`()
+
+fun <T> Iterable<T>.`should not be empty`() = assertNotEmpty(this, "Iterable")
+fun <T> Iterable<T>.shouldNotBeEmpty() = this.`should not be empty`()
 
 infix fun <R, T> Map<R, T>.`should have key`(theKey: R) = if (this.containsKey(theKey)) Unit else fail("$this should contain key $theKey", "$theKey", join(this.keys))
 infix fun <R, T> Map<R, T>.shouldHaveKey(theKey: R) = this `should have key` theKey
@@ -39,6 +50,12 @@ infix fun <R, T> Map<R, T>.shouldContain(thePair: Pair<R, T>) = this `should con
 infix fun <R, T> Map<R, T>.`should not contain`(theThing: Pair<R, T>) = if (this[theThing.first] != theThing.second) Unit else fail("$this should not contain $theThing", "the map to not contain the pair $theThing", join(this))
 infix fun <R, T> Map<R, T>.shouldNotContain(thePair: Pair<R, T>) = this `should not contain` thePair
 
+fun <R, T> Map<R, T>.`should be empty`() = assertEmpty(this.toList(), "Map")
+fun <R, T> Map<R, T>.shouldBeEmpty() = this.`should be empty`()
+
+fun <R, T> Map<R, T>.`should not be empty`() = assertNotEmpty(this.toList(), "Map")
+fun <R, T> Map<R, T>.shouldNotBeEmpty() = this.`should not be empty`()
+
 infix fun <T> Any?.`should not be in`(array: Array<T>) = if (!array.contains(this)) Unit else fail("$this should not be in $array", "the value $this to not be in the Array", join(array))
 infix fun <T> Any?.shouldNotBeIn(array: Array<T>) = this `should not be in` array
 
@@ -51,4 +68,5 @@ infix fun <T> Any?.shouldNotBeIn(iterable: Iterable<T>) = this `should not be in
 infix fun <T> Any?.`should be in`(array: Array<T>) = if (array.contains(this)) Unit else fail("$this should be in $array", "$this", join(array))
 infix fun <T> Any?.shouldBeIn(array: Array<T>) = this `should be in` array
 
-
+private fun <T> assertEmpty(iterable: Iterable<T>, collectionType: String) = assertTrue("Expected the $collectionType to be empty, but has ${iterable.count()} elements", iterable.count() == 0)
+private fun <T> assertNotEmpty(iterable: Iterable<T>, collectionType: String) = assertTrue("Expected the $collectionType to contain no elements", iterable.count() > 0)
