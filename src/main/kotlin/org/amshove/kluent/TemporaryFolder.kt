f103711ac49bcd4546314aedcee2f4af06571550
Markus Amshove <scm@amshove.org>
2017-04-17 18:16:29 +0200

    Add TemporaryFolder builder

---
+++ src/main/kotlin/org/amshove/kluent/TemporaryFolder.kt
@@ -0,0 +1,81 @@
+package org.amshove.kluent
+
+import org.junit.rules.TemporaryFolder
+import java.io.File
+import java.nio.charset.Charset
+import java.nio.file.Paths
+
+fun temporaryFolder(init: TemporaryFolderBuilder.() -> Unit): TemporaryFolder {
+    val folder = TemporaryFolder()
+    folder.create()
+    val builder = TemporaryFolderBuilder(folder)
+    init(builder)
+    return folder
+}
+
+class TemporaryFolderBuilder(private val temporaryFolder: TemporaryFolder) {
+    fun dir(name: String, init: FolderBuilder.() -> Unit): FolderBuilder {
+        val folder = createFolder(name)
+        val folderBuilder = FolderBuilder(folder)
+        init(folderBuilder)
+        return folderBuilder
+    }
+
+    fun file(name: String, init: FileBuilder.() -> Unit): FileBuilder {
+        val file = temporaryFolder.newFile(name)
+        val fileBuilder = FileBuilder(file)
+        file.createNewFile()
+        init(fileBuilder)
+        return fileBuilder
+    }
+
+    private fun createFolder(name: String) : File {
+        if(name.contains("/")) {
+            var currentPath = temporaryFolder.root.toPath()
+            var lastFile = File(currentPath.toUri())
+            name.split("/").forEach {
+                lastFile.mkdir()
+                currentPath = Paths.get(currentPath.toString(), it)
+                lastFile = File(currentPath.toUri())
+            }
+
+            lastFile.mkdir()
+            lastFile.mkdir()
+            return lastFile
+        }
+        else {
+            return temporaryFolder.newFolder(name)
+        }
+    }
+}
+
+class FolderBuilder(private val folder: File) {
+    fun dir(name: String, init: FolderBuilder.() -> Unit): FolderBuilder {
+        val folder = File(folder, name)
+        val folderBuilder = FolderBuilder(folder)
+        folder.mkdir()
+        init(folderBuilder)
+        return folderBuilder
+    }
+
+    fun file(name: String, init: FileBuilder.() -> Unit): FileBuilder {
+        val file = File(folder, name)
+        val fileBuilder = FileBuilder(file)
+        file.createNewFile()
+        init(fileBuilder)
+        return fileBuilder
+    }
+}
+
+class FileBuilder(private val file: File) {
+    fun content(charset: Charset = Charset.defaultCharset(), content: () -> String) {
+        val theContent = content()
+        file.writeText(theContent, charset)
+    }
+
+    fun trimContent(charset: Charset = Charset.defaultCharset(), content: () -> String) {
+        val theContent = content().trimIndent()
+        file.writeText(theContent, charset)
+    }
+
+}
