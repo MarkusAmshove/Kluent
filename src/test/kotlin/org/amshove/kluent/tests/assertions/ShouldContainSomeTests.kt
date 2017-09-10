010314728c99d637dc9d2b9d14ee46b049c547c4
Markus Amshove <scm@amshove.org>
2017-09-10 09:54:21 +0200

    Add shouldContainSome and shouldContainNone to collections

---
+++ src/test/kotlin/org/amshove/kluent/tests/assertions/ShouldContainSomeTests.kt
@@ -0,0 +1,26 @@
+package org.amshove.kluent.tests.assertions
+
+import org.amshove.kluent.shouldContainSome
+import org.jetbrains.spek.api.Spek
+import kotlin.test.assertFails
+
+class ShouldContainSomeTests : Spek({
+    given("the shouldContainSome method") {
+        on("testing a list which contains at least one element") {
+            it("should pass") {
+                val cities = listOf("Israel", "Berlin", "Phoenix", "Egypt")
+                val actual = listOf("Berlin", "Stuttgart")
+
+                actual shouldContainSome cities
+            }
+        }
+        on("testing a list which doesn't contain at least one element") {
+            it("should fail") {
+                val cities = listOf("Israel", "Phoenix", "Egypt")
+                val actual = listOf("Berlin", "Stuttgart")
+
+                assertFails { actual shouldContainSome cities }
+            }
+        }
+    }
+})
