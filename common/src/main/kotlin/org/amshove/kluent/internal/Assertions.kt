310aa901fd9b75cf39b37c47d8c75bfcb2ebfba9
Markus Amshove <scm@amshove.org>
2018-05-30 18:08:52 +0200

    Temporary implement assertSame and assertNotSame

--- common/src/main/kotlin/org/amshove/kluent/internal/Assertions.kt
+++ common/src/main/kotlin/org/amshove/kluent/internal/Assertions.kt
@@ -6,3 +6,12 @@ import kotlin.test.assertFalse
 fun assertTrue(message: String, boolean: Boolean) = assertTrue(boolean, message)
 
 fun assertFalse(message: String, boolean: Boolean) = assertFalse(boolean, message)
+
+// TODO: assertSame and notSame currently not implemented in native 0.6
+fun assertSame(expected: Any?, actual: Any?) {
+    assertTrue("Expected <$expected>, actual <$actual> are not the same instance.", actual === expected)
+}
+
+fun assertNotSame(expected: Any?, actual: Any?) {
+    assertTrue("Expected <$expected>, actual <$actual> are the same instance.", actual !== expected)
+}
