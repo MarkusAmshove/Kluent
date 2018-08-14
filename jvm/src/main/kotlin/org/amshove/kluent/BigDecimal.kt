aeae16e3a79e97ccb7f3426f11d4c4b9cfb8acad
Markus Amshove <scm@amshove.org>
2018-08-14 19:11:23 +0200

    Change operators to compareTo

--- jvm/src/main/kotlin/org/amshove/kluent/BigDecimal.kt
+++ jvm/src/main/kotlin/org/amshove/kluent/BigDecimal.kt
@@ -4,25 +4,25 @@ import org.amshove.kluent.internal.assertTrue
 import java.math.BigDecimal
 
 infix fun BigDecimal.shouldEqualTo(expected: BigDecimal) =
-    this.apply { assertTrue("Expected $this to be equal to $expected", this == expected) }
+    this.apply { assertTrue("Expected $this to be equal to $expected", this.compareTo(expected) == 0) }
 
 infix fun BigDecimal.shouldNotEqualTo(expected: BigDecimal) =
-    this.apply { assertTrue("Expected $this to not be equal to $expected", this != expected) }
+    this.apply { assertTrue("Expected $this to not be equal to $expected", this.compareTo(expected) != 0) }
 
 infix fun BigDecimal.shouldBeGreaterThan(expected: BigDecimal) =
-    this.apply { assertTrue("Expected $this to be greater than $expected", this > expected) }
+    this.apply { assertTrue("Expected $this to be greater than $expected", this.compareTo(expected) > 0) }
 
 infix fun BigDecimal.shouldNotBeGreaterThan(expected: BigDecimal) =
-    this.apply { assertTrue("Expected $this to not be greater than $expected", this <= expected) }
+    this.apply { assertTrue("Expected $this to not be greater than $expected", this.compareTo(expected) <= 0) }
 
 infix fun BigDecimal.shouldBeGreaterOrEqualTo(expected: BigDecimal) =
-    this.apply { assertTrue("Expected $this to be greater or equal to $expected", this >= expected) }
+    this.apply { assertTrue("Expected $this to be greater or equal to $expected", this.compareTo(expected) >= 0) }
 
 infix fun BigDecimal.shouldNotBeGreaterOrEqualTo(expected: BigDecimal) =
-    this.apply { assertTrue("Expected $this to not be greater or equal to $expected", this < expected) }
+    this.apply { assertTrue("Expected $this to not be greater or equal to $expected", this.compareTo(expected) < 0) }
 
 infix fun BigDecimal.shouldBeLessThan(expected: BigDecimal) =
-    this.apply { assertTrue("Expected $this to be less than $expected", this < expected) }
+    this.apply { assertTrue("Expected $this to be less than $expected", this.compareTo(expected) < 0) }
 
 infix fun BigDecimal.shouldNotBeLessThan(expected: BigDecimal) =
-    this.apply { assertTrue("Expected $this to not be less than $expected", this >= expected) }
+    this.apply { assertTrue("Expected $this to not be less than $expected", this.compareTo(expected) >= 0) }
--- jvm/src/test/kotlin/org/amshove/kluent/tests/assertions/bigdecimal/BigDecimalShouldEqualToShould.kt
+++ jvm/src/test/kotlin/org/amshove/kluent/tests/assertions/bigdecimal/BigDecimalShouldEqualToShould.kt
@@ -2,6 +2,7 @@ package org.amshove.kluent.tests.assertions.bigdecimal
 
 import org.amshove.kluent.shouldEqualTo
 import java.math.BigDecimal
+import java.math.BigInteger
 import kotlin.test.Test
 import kotlin.test.assertFails
 
@@ -12,6 +13,14 @@ class BigDecimalShouldEqualToShould {
     fun passWhenComparingTwoEqualValues() {
         val a = BigDecimal(1925112616126126)
         val b = BigDecimal(1925112616126126)
+        a.shouldEqualTo(b)
+    }
+
+    @Test
+    fun passWhenComparingTwoEqualValuesWithDifferentScale() {
+        val a = BigDecimal(BigInteger.valueOf(1), -1) // 10
+        val b = BigDecimal(BigInteger.valueOf(10)) // 10
+
         a.shouldEqualTo(b)
     }
 
