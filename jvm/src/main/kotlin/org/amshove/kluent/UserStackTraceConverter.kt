f218275e769f937231435c9f5f5fbdf268a4210f
Maxim Ivanov <drcolombo@gmail.com>
2020-11-22 18:07:27 +0100

    Small refactoring referencing "Kluent" instead of "Kotest"

--- jvm/src/main/kotlin/org/amshove/kluent/UserStackTraceConverter.kt
+++ jvm/src/main/kotlin/org/amshove/kluent/UserStackTraceConverter.kt
@@ -7,36 +7,36 @@ object UserStackTraceConverter {
     }
 
     /**
-     * Drops stacktraces until it finds a Kotest Stacktrace then drops stacktraces until it finds a non-Kotest stacktrace
+     * Drops stacktraces until it finds a Kluent Stacktrace then drops stacktraces until it finds a non-Kluent stacktrace
      *
-     * Sometimes, it's possible for the Stacktrace to contain classes that are not from Kotest,
-     * such as classes from sun.reflect or anything from Java. After clearing these classes, we'll be at Kotest
+     * Sometimes, it's possible for the Stacktrace to contain classes that are not from Kluent,
+     * such as classes from sun.reflect or anything from Java. After clearing these classes, we'll be at Kluent
      * stacktrace, which will contain exceptions from the Runners and some other classes
-     * After everything from Kotest we'll finally be at user classes, at which point the stacktrace is clean and is
+     * After everything from Kluent we'll finally be at user classes, at which point the stacktrace is clean and is
      * returned.
      */
     private fun Array<StackTraceElement>.dropUntilUserClass(): Array<StackTraceElement> {
-        return toList().dropUntilFirstKotestClass().dropUntilFirstNonKotestClass().toTypedArray()
+        return toList().dropUntilFirstKluentClass().dropUntilFirstNonKluentClass().toTypedArray()
     }
 
-    private fun List<StackTraceElement>.dropUntilFirstKotestClass(): List<StackTraceElement> {
+    private fun List<StackTraceElement>.dropUntilFirstKluentClass(): List<StackTraceElement> {
         return dropWhile {
-            it.isNotKotestClass()
+            it.isNotKluentClass()
         }
     }
 
-    private fun List<StackTraceElement>.dropUntilFirstNonKotestClass(): List<StackTraceElement> {
+    private fun List<StackTraceElement>.dropUntilFirstNonKluentClass(): List<StackTraceElement> {
         return dropWhile {
-            it.isKotestClass()
+            it.isKluentClass()
         }
     }
 
-    private fun StackTraceElement.isKotestClass(): Boolean {
+    private fun StackTraceElement.isKluentClass(): Boolean {
         return className.startsWith("org.amshove.kluent")
     }
 
-    private fun StackTraceElement.isNotKotestClass(): Boolean {
-        return !isKotestClass()
+    private fun StackTraceElement.isNotKluentClass(): Boolean {
+        return !isKluentClass()
     }
 
 }
